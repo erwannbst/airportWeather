3e6225208639446860a03cd62f21081348ee6ef4
Maël SUARD <maelsuard@orange.fr>
2022-01-11 21:21:05 +0100

    get measures by type done

--- api/controllers/airportWeather_controller.go
+++ api/controllers/airportWeather_controller.go
@@ -14,20 +14,39 @@ import (
 
 var sensorCollection *mongo.Collection = configs.GetCollection(configs.DB, "sensor")
 
-func GetAMeasure(c *fiber.Ctx) error {
+func GetMeasureByType(c *fiber.Ctx) error {
+
+	measureType := c.Query("measure_type")
+	startDate := c.Query("start_date")
+	endDate := c.Query("end_date")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	measureType := c.Params("measure_type")
-	//from := c.Params("from")
-	//to := c.Params("to")
-	var measure models.AirportInfo
+	var measures []models.AirportInfo
 	defer cancel()
 
-	err := sensorCollection.FindOne(ctx, bson.M{"MeasureType": measureType}).Decode(&measure)
+	//db.sensor.aggregate([{$match:{"MeasureType":"Wind speed","Time":{$gte:"2022-01-09",$lte:"2022-01-10"}}}])
+
+	matchStage := bson.D{{
+		"$match", bson.D{{"MeasureType", measureType},{"Time",bson.D{{"$gte",startDate},{"$lte", endDate}}}}}}
+
+	results ,err := sensorCollection.Aggregate(ctx, mongo.Pipeline{matchStage})
 	if err != nil {
 		return c.Status(http.StatusInternalServerError).JSON(responses.AirportWeatherResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
 	}
 
-	return c.Status(http.StatusOK).JSON(responses.AirportWeatherResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": measure}})
+	//reading from the db in an optimal way
+	defer results.Close(ctx)
+	for results.Next(ctx) {
+		var singleMeasure models.AirportInfo
+		if err = results.Decode(&singleMeasure); err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(responses.AirportWeatherResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+		}
+
+		measures = append(measures, singleMeasure)
+
+	}
+
+	return c.Status(http.StatusOK).JSON(
+		responses.AirportWeatherResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": measures}})
 }
 
 func GetAllMeasures(c *fiber.Ctx) error {
@@ -35,9 +54,12 @@ func GetAllMeasures(c *fiber.Ctx) error {
 	var measures []models.AverageMeasuresInfo
 	defer cancel()
 
-	day := c.Params("day")
+
+
+	day := c.Query("day")
+
 	matchStage := bson.D{{
-		"$match", bson.D{{"Time", bson.M{"$gte": day+"-00-00-00", "$lte": day+"23-59-59"}}}}}
+		"$match", bson.D{{"Time", bson.M{"$gte": day+"-00-00-00", "$lte": day+"-23-59-59"}}}}}
 	groupStage := bson.D{{"$group", bson.D{{"_id", "$MeasureType"}, {"AvgValue", bson.D{{"$avg", "$Value"}}}}}}
 	results, err := sensorCollection.Aggregate(ctx, mongo.Pipeline{matchStage,groupStage})
 
--- api/routes/airportWeather_routes.go
+++ api/routes/airportWeather_routes.go
@@ -7,7 +7,7 @@ import (
 )
 
 func AirportRoute(app *fiber.App) {
-	app.Get("/airport_weather/measure_by_type", controllers.GetAMeasure)
+	app.Get("/airport_weather/measures_by_type", controllers.GetMeasureByType)
 	app.Get("/airport_weather/average", controllers.GetAllMeasures)
 }
 
