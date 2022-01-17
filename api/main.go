e10869f5741d1b23da2d751587cfe1fa6e5bcb53
Maël SUARD <maelsuard@orange.fr>
2022-01-17 14:09:35 +0100

    ajout cors

--- api/main.go
+++ api/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"api/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,PATCH",
+	}))
 
 	routes.AirportRoute(app)
 
